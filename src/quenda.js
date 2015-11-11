150d84b87835b5a8fc06d7e31b7ee9e7f584855a
Berto Yáñez <berto@ber.to>
2015-11-11 18:01:35 +0100

    Bugfixes 1.0.3

--- src/quenda.js
+++ src/quenda.js
@@ -11,168 +11,163 @@
 }(this, function(merge) {
     'use strict';
 
+    var Queue = {
+
+        /**
+         * Adds a new step to this queue
+         * @param {Object} Step definition
+         * @return {Object} This queue instance
+         */
+        add: function(step) {
+            if (!step) {
+                throw new Error('Not enough arguments');
+            }
+
+            if (Array.isArray(step)) {
+                step.forEach(function(s) {
+                    this.add(s);
+                }, this);
+            } else {
+                if (step.nextDelay && step.nextDelay < 0) {
+                    throw new Error('nextDelay property must be greater than 0');
+                }
+
+                if (step.fn && typeof(step.fn) !== 'function') {
+                    throw new Error('The property fn must be a function');
+                }
+
+                if (step.autoDestroy && typeof(step.autoDestroy) !== 'boolean') {
+                    throw new Error('The property autoDestroy must be a boolean');
+                }
+
+                if (step.preload && !Array.isArray(step.preload)) {
+                    throw new Error('The property preload must be an array of urls');
+                }
+
+                this.steps.push(step);
+            }
+            return this;
+        },
+        /**
+         * Plays this queue
+         * @return {Object} This queue instance
+         */
+        play: function() {
+            if (!this._playing) {
+                this._playing = true;
+                this._executeStep(this._current);
+            }
+
+            return this;
+        },
+        /**
+         * Pauses this queue
+         * @return {Object} This queue instance
+         */
+        pause: function() {
+            if (this._currentTimeout) {
+                clearTimeout(this._currentTimeout);
+            }
+
+            this._playing = false;
+
+            return this;
+        },
+        /**
+         * Moves to next element in this queue
+         * @return {Object} This queue instance
+         */
+        next: function() {
+            this.pause();
+            this._executeStep(++this._current);
+
+            return this;
+        },
+        /**
+         * Moves to the previous element in this queue
+         * @return {Object} This queue instance
+         */
+        prev: function() {
+            this.pause();
+            this._executeStep(--this._current);
+
+            return this;
+        },
+        _current: 0,
+        _loops: 0,
+        _playing: false,
+        /**
+         * Executes one element of the queue
+         * @param  {number} index the element index
+         */
+        _executeStep: function(index) {
+            var step;
+
+            if (!this.steps[index]) {
+                if (this.config.loop && this._loops < this.config.maxLoops) {
+                    this._loops++;
+                    step = this.steps[this._current = 0];
+                } else {
+                    return false;
+                }
+            } else {
+                step = this.steps[index];
+            }
+
+            if (step.preload && !step.preloaded) {
+                this._handlePreload(step.preload, function() {
+                    step.preloaded = true;
+                    this._setNext(step.nextDelay);
+                    step.fn && step.fn.call(this, step);
+                }.bind(this));
+            } else {
+                this._setNext(step.nextDelay);
+                step.fn && step.fn.call(this, step);
+            }
+
+            if (step.autoDestroy) {
+                var i = this.steps.indexOf(step);
+                this._current = i;
+                this.steps.splice(i, 1);
+            }
+        },
+        /**
+         * Sets the execution timeout of the next element in the queue
+         * @param {number} nextDelay the delay in ms
+         */
+        _setNext: function(nextDelay) {
+            var delay = nextDelay || this.config.defaultDelay;
+
+            if (delay && this._playing) {
+                this._currentTimeout = setTimeout(function() {
+                    this._executeStep(++this._current);
+                }.bind(this), delay);
+            }
+        },
+        /**
+         * Handles the image preload
+         * @param  {Array}   images   array of image urls
+         * @param  {Function} callback callback to execute after all the images are loaded
+         */
+        _handlePreload: function(images, callback) {
+            var loaded = 0;
+            images.forEach(function(src) {
+                var image = new Image();
+                image.onload = function() {
+                    ++loaded;
+                    loaded === images.length && callback();
+                };
+                image.src = src;
+            });
+        }
+    };
+
     /**
      * Quenda object
      * @type {Object}
      */
     var Quenda = {
         queues: [],
-        prototype: {
-
-            /**
-             * Queues API
-             */
-
-            /**
-             * Adds a new step to this queue
-             * @param {Object} Step definition
-             * @return {Object} This queue instance
-             */
-            add: function(step) {
-                if (!step) {
-                    throw new Error('Not enough arguments');
-                }
-
-                if (Array.isArray(step)) {
-                    step.forEach(function(s) {
-                        this.add(s);
-                    }, this);
-                } else {
-                    if (step.nextDelay && step.nextDelay < 0) {
-                        throw new Error('nextDelay property must be greater than 0');
-                    }
-
-                    if (step.fn && typeof(step.fn) !== 'function') {
-                        throw new Error('The property fn must be a function');
-                    }
-
-                    if (step.autoDestroy && typeof(step.autoDestroy) !== 'boolean') {
-                        throw new Error('The property autoDestroy must be a boolean');
-                    }
-
-                    if (step.preload && !Array.isArray(step.preload)) {
-                        throw new Error('The property preload must be an array of urls');
-                    }
-
-                    this.steps.push(step);
-                }
-                return this;
-            },
-            /**
-             * Plays this queue
-             * @return {Object} This queue instance
-             */
-            play: function() {
-                // if(!this.playing) {
-                this._execute(this._current);
-                //     this.playing = true;
-                // }
-
-                return this;
-            },
-            /**
-             * Pauses this queue
-             * @return {Object} This queue instance
-             */
-            pause: function() {
-                if (this._currentTimeout) {
-                    clearTimeout(this._currentTimeout);
-                }
-
-                // this.playing = false;
-
-                return this;
-            },
-            /**
-             * Moves to next element in this queue
-             * @return {Object} This queue instance
-             */
-            next: function() {
-                this.pause();
-                this._current++;
-                this.play();
-
-                return this;
-            },
-            /**
-             * Moves to the previous element in this queue
-             * @return {Object} This queue instance
-             */
-            prev: function() {
-                this.pause();
-                this._current--;
-                this.play();
-
-                return this;
-            },
-            _current: 0,
-            loops: 0,
-            playing: false,
-            /**
-             * Executes one element of the queue
-             * @param  {number} index the element index
-             */
-            _execute: function(index) {
-                var step;
-
-                if (!this.steps[index]) {
-                    if (this.config.loop && this.loops < this.config.maxLoops) {
-                        this.loops++;
-                        step = this.steps[this._current = 0];
-                    } else {
-                        return false;
-                    }
-                } else {
-                    step = this.steps[this._current];
-                }
-
-                if (step.preload && !step.preloaded) {
-                    this._handlePreload(step.preload, function() {
-                        this._setNextTimeout(step.nextDelay);
-                        step.preloaded = true;
-                        step.fn && step.fn.call(this, step);
-                    }.bind(this));
-                } else {
-                    this._setNextTimeout(step.nextDelay);
-                    step.fn && step.fn.call(this, step);
-                }
-
-                if (step.autoDestroy) {
-                    var i = this.steps.indexOf(step);
-                    this._current = i;
-                    this.steps.splice(i, 1);
-                }
-            },
-            /**
-             * Sets the execution timeout of the next element in the queue
-             * @param {number} nextDelay the delay in ms
-             */
-            _setNextTimeout: function(nextDelay) {
-                var delay = nextDelay || this.config.defaultDelay;
-
-                if (delay) {
-                    this._currentTimeout = setTimeout(function() {
-                        this._execute(++this._current);
-                    }.bind(this), delay);
-                }
-            },
-            /**
-             * Handles the image preload
-             * @param  {Array}   images   array of image urls
-             * @param  {Function} callback callback to execute after all the images are loaded
-             */
-            _handlePreload: function(images, callback) {
-                var loaded = 0;
-                images.forEach(function(src) {
-                    var image = new Image();
-                    image.onload = function() {
-                        ++loaded;
-                        loaded === images.length && callback();
-                    };
-                    image.src = src;
-                });
-            }
-        },
         /**
          * Gets all the queues stored in the main object
          * @return {Array} Array of queue instances
@@ -219,7 +214,7 @@
                 maxLoops: Infinity
             };
 
-            var instance = Object.create(this.prototype);
+            var instance = Object.create(Queue);
 
             this.queues.push(name ? {
                 name: name,
@@ -237,7 +232,7 @@
         }
     };
 
-    Quenda.fn = Quenda.prototype;
+    Quenda.fn = Quenda.prototype = Queue;
 
     Quenda.version = 'VERSION';
 
